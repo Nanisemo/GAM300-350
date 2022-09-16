744d8ad43cf8855ced2ef5b30b8393e37c36ed01
Louventia <ncp1998@hotmail.com>
2022-09-17 03:32:20 +0800

    Ability System Up

---
+++ GAM-300-Prototype/Assets/Scripts/System/Ability/AbilityCooldown.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class AbilityCooldown : MonoBehaviour
+{
+    public string abilityButtonAxisName = "Fire1";
+    public Image darkMask;
+    public TMP_Text coolDownTextDisplay;
+
+    [SerializeField] private Ability ability;
+    [SerializeField] private GameObject weaponHolder;
+
+    private Image myButtonImage;
+    // private AudioSource abilitySource;
+
+    private float 
+        coolDownDuration, 
+        nextReadyTime, 
+        coolDownTimeLeft;
+
+
+    void Start()
+    {
+        Initialize(ability, weaponHolder);
+    }
+
+    public void Initialize(Ability selectedAbility, GameObject weaponHolder)
+    {
+        ability = selectedAbility;
+        myButtonImage = GetComponent<Image>();
+        // abilitySource = GetComponent<AudioSource>();
+        myButtonImage.sprite = ability.abilitySprite;
+        darkMask.sprite = ability.abilitySprite;
+        coolDownDuration = ability.abilityBaseCoolDown;
+        ability.Initialize(weaponHolder);
+        AbilityReady();
+    }
+
+    void Update()
+    {
+        bool coolDownComplete = (Time.time > nextReadyTime);
+        if (coolDownComplete)
+        {
+            AbilityReady();
+            if (Input.GetButtonDown(abilityButtonAxisName))
+            {
+                ButtonTriggered();
+            }
+        }
+        else
+        {
+            CoolDown();
+        }
+    }
+
+    private void AbilityReady()
+    {
+        coolDownTextDisplay.enabled = false;
+        darkMask.enabled = false;
+    }
+
+    private void CoolDown()
+    {
+        coolDownTimeLeft -= Time.deltaTime;
+        float roundedCd = Mathf.Round(coolDownTimeLeft);
+        coolDownTextDisplay.text = roundedCd.ToString();
+        darkMask.fillAmount = (coolDownTimeLeft / coolDownDuration);
+    }
+
+    private void ButtonTriggered()
+    {
+        nextReadyTime = coolDownDuration + Time.time;
+        coolDownTimeLeft = coolDownDuration;
+        darkMask.enabled = true;
+        coolDownTextDisplay.enabled = true;
+
+        //abilitySource.clip = ability.abilitySound;
+        //abilitySource.Play();
+        ability.TriggerAbility();
+    }
+}
