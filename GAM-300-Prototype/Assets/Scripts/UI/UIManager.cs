8e0bf2444a21f56c9e399f63a3bca3f20c6bde34
Louventia <ncp1998@hotmail.com>
2022-10-17 01:17:47 +0800

    Wehh

--- GAM-300-Prototype/Assets/Scripts/UI/UIManager.cs
+++ GAM-300-Prototype/Assets/Scripts/UI/UIManager.cs
@@ -6,60 +6,13 @@ using TMPro;
 
 public class UIManager : MonoBehaviour
 {
-    private bool
-        isCooldown = false;
-
-    public float
-        cooldownTime = 5.0f,
-        cooldownTimer = 0.0f;
-
-    [SerializeField]
-    private Image
-        abilityCooldown;
-
-    [SerializeField]
-    private TMP_Text ability_cooldownText;
-
-    private void Start()
-    {
-        ability_cooldownText.gameObject.SetActive(false);
-
-        abilityCooldown.fillAmount = 0.0f;
-    }
+    [SerializeField] private GameObject gui;
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.Q))
-        {
-            Debug.Log("User pressed Q");
-            isCooldown = true;
-            cooldownTimer = cooldownTime;
-            cooldownTimer -= Time.deltaTime;
-
-            if (cooldownTimer <= 0.0f)
-            {
-                isCooldown = false;
-                cooldownTimer = 0.0f;
-                ability_cooldownText.gameObject.SetActive(false);
-                abilityCooldown.fillAmount = 0.0f;
-            } 
-            else {
-                abilityCooldown.fillAmount = cooldownTimer / cooldownTime;
-            }
-        }
-    }
-
-    public void UseSpell()
-    {
-        if (isCooldown)
-        {
-            Debug.Log("user cast an ability");
-        }
+        if (GlobalBool.isGameOver)
+            gui.SetActive(false);
         else
-        {
-            isCooldown = true;
-            ability_cooldownText.gameObject.SetActive(true);
-            cooldownTimer = cooldownTime;
-        }
+            gui.SetActive(true);
     }
 }
