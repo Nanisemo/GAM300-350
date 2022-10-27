0fccea20ee6cd4df8ea0dca9a87802359144d2cc
Nanisemo <77495343+Nanisemo@users.noreply.github.com>
2022-10-27 13:10:13 +0800

    camera and poison tweaks

--- GAM-300-Prototype/Assets/Scripts/System/CameraShake.cs
+++ GAM-300-Prototype/Assets/Scripts/System/CameraShake.cs
@@ -10,7 +10,7 @@ public class CameraShake : MonoBehaviour
     void Start()
     {
         source = GetComponent<CinemachineImpulseSource>();
-        Invoke("ShakeCamera", 3f);
+       // Invoke("ShakeCamera", 3f);
     }
 
     public void ShakeCamera()
